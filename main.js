34c8cfd97f894d9df54a622bcfa6cdfda9261134
Code Adventures <86422733+Codedventures@users.noreply.github.com>
2021-07-07 12:26:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function Paint(){
+    window.location="paint.html";
+}
+function Games(){
+    window.location="games.html";
+}
+function MySite(){
+    window.location="Krishnapoleboyina'sPersonalWebsite_WhiteHatJr.html";
+}
+function VoiceSelfie(){
+    window.location="voiceselfie.html";
+}
